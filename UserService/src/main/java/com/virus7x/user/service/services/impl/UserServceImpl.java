e85fb8e238200160f6f4dfd0f9a70ada81cf89fb
aymane biti <aymanbiti2@gmail.com>
2023-07-26 12:18:37 -0700

    feignclient

--- UserService/src/main/java/com/virus7x/user/service/services/impl/UserServceImpl.java
+++ UserService/src/main/java/com/virus7x/user/service/services/impl/UserServceImpl.java
@@ -4,6 +4,7 @@ import com.virus7x.user.service.entities.Hotel;
 import com.virus7x.user.service.entities.Rating;
 import com.virus7x.user.service.entities.User;
 import com.virus7x.user.service.exceptions.ResourceNotFoundException;
+import com.virus7x.user.service.external.service.HotelService;
 import com.virus7x.user.service.repositories.UserRepository;
 import com.virus7x.user.service.services.UserService;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
@@ -25,6 +26,7 @@ public class UserServceImpl implements UserService {
 
     private final UserRepository repository;
     private final RestTemplate restTemplate;
+    private final HotelService hotelService;
     @Override
     public User saveUser(User user) {
         String randomUserId = UUID.randomUUID().toString();
@@ -34,6 +36,7 @@ public class UserServceImpl implements UserService {
 
     @Override
     public List<User> getAllUser() {
+
         return repository.findAll();
     }
 
@@ -41,12 +44,11 @@ public class UserServceImpl implements UserService {
     public User getUser(String userId) {
         User user = repository.findById(userId).orElseThrow(()-> new ResourceNotFoundException("User with given id is not found on server!! : "+userId));
 
-        Rating[] ratingForUser = restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getUserId(), Rating[].class);
+        Rating[] ratingForUser = restTemplate.getForObject("http://RATING-SERVICE/ratings/users/"+user.getUserId(), Rating[].class);
         List<Rating> ratings = Arrays.stream(ratingForUser).toList();
         List<Rating> ratingList = ratings.stream().map(rating -> {
 //            http://localhost:8082/hotels/94b4762d-0139-42f5-b76a-189cc4d58cc1
-
-            ResponseEntity<Hotel> forEntity = restTemplate.getForEntity("http://localhost:8082/hotels/"+rating.getHotelId(), Hotel.class);
+            ResponseEntity<Hotel> forEntity = restTemplate.getForEntity("http://HOTEL-SERVICE/hotels/"+rating.getHotelId(), Hotel.class);
                 Hotel body = forEntity.getBody();
             log.info("status {}",forEntity.getStatusCode());
             rating.setHotel(body);
