48300e3e1f9f36b93ffb70d7d185ca615990cb72
aymane biti <aymanbiti2@gmail.com>
2023-08-07 03:51:57 -0700

    security service user

---
+++ ApiGatway/src/main/java/com/virus7x/apigatway/controller/AuthController.java
@@ -0,0 +1,50 @@
+package com.virus7x.apigatway.controller;
+
+import com.virus7x.apigatway.model.AuthResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/auth")
+@Slf4j
+public class AuthController {
+
+
+    @GetMapping("/login")
+    public ResponseEntity<AuthResponse> login(
+            @RegisteredOAuth2AuthorizedClient("okta") OAuth2AuthorizedClient client,
+            @AuthenticationPrincipal OidcUser user,
+            Model model
+
+    ){
+
+        log.info("user email id: {}",user.getEmail());
+
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setUserId(user.getEmail());
+        authResponse.setAccessToken(client.getAccessToken().getTokenValue());
+        authResponse.setRefreshToken(client.getRefreshToken().getTokenValue());
+        authResponse.setExpireAt(client.getAccessToken().getExpiresAt().getEpochSecond());
+        List<String> authorities = user.getAuthorities().stream().map(
+                grantedAuthority -> {
+                    return grantedAuthority.getAuthority();
+                }
+        ).collect(Collectors.toList());
+
+        authResponse.setAuthorities(authorities);
+        return new ResponseEntity<>(authResponse, HttpStatus.OK);
+    }
+}
