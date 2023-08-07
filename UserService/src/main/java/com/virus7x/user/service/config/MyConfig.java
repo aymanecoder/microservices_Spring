72b146d84a7100b6a64c503b1667861d707b71b2
aymane biti <aymanbiti2@gmail.com>
2023-08-07 07:10:49 -0700

    security user Service

--- UserService/src/main/java/com/virus7x/user/service/config/MyConfig.java
+++ UserService/src/main/java/com/virus7x/user/service/config/MyConfig.java
@@ -1,15 +1,58 @@
 package com.virus7x.user.service.config;
 
+import com.virus7x.user.service.config.interceptor.RestTemplateInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
+@Component
+@RequiredArgsConstructor
 public class MyConfig {
+    private final ClientRegistrationRepository clientRegistrationRepository;
+    private final OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository;
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate(){
-        return new RestTemplate();
+
+        RestTemplate restTemplate = new RestTemplate();
+        List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+        interceptors.add(new RestTemplateInterceptor(manager(
+            clientRegistrationRepository,
+                oAuth2AuthorizedClientRepository
+        )));
+        restTemplate.setInterceptors(interceptors);
+        return restTemplate;
+    }
+
+
+    @Bean
+    public OAuth2AuthorizedClientManager manager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository auth2AuthorizedClientRepository
+    ){
+
+        OAuth2AuthorizedClientProvider provider = OAuth2AuthorizedClientProviderBuilder.
+                builder()
+                .clientCredentials()
+                .build();
+        DefaultOAuth2AuthorizedClientManager defaultOAuth2AuthorizedClientManager = new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository,auth2AuthorizedClientRepository);
+        defaultOAuth2AuthorizedClientManager.setAuthorizedClientProvider(provider);
+        return defaultOAuth2AuthorizedClientManager;
+
     }
 }
